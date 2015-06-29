937456527eafb8282df25809f64a7cea1a0ef6d1
gerbias <smitnich@onid.orst.edu>
2015-06-28 21:44:25 -0700

    Adding code to absorb color from the existing floor.

---
+++ Assets/AbsorbColor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbsorbColor : MonoBehaviour {
+    int red, green, blue;
+    SetColor colorScript;
+    BlitColors blitScript;
+	// Use this for initialization
+	void Start () {
+        colorScript = GetComponent<SetColor>();
+        Color tmpColor = colorScript.color;
+        red = (int) (tmpColor.r*255);
+        green = (int) (tmpColor.g * 255);
+        blue = (int) (tmpColor.b * 255);
+	}
+    Color determineColor()
+    {
+        return new Color((float) (red/255.0), (float) (green/255.0), (float) (blue/255.0));
+    }
+	// Update is called once per frame
+	void Update () {
+        //blitScript.setColor(transform.position, transform.position, Color.white, 1, 0);
+        colorScript.ChangeColor(determineColor());
+	}
+}
