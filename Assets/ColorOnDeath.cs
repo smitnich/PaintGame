454b20b0556078efbcf0b055e866a50a94eed67c
gerbias <smitnich@onid.orst.edu>
2015-07-01 17:30:39 -0700

    Adding script to leave behind color splotches when an object is destroyed.

---
+++ Assets/ColorOnDeath.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorOnDeath : MonoBehaviour {
+    FloorManager script;
+    int size;
+    Color color;
+	// Use this for initialization
+	void Start () {
+        GameObject floor = GameObject.Find("Floor");
+        Vector3 extents = GetComponent<Renderer>().bounds.extents;
+        script = floor.GetComponent<FloorManager>();
+        color = GetComponent<SetColor>().color;
+        size = script.determineSize(extents.x);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnDestroy()
+    {
+        if (script != null)
+            script.setColor(transform.position, transform.position, color, size);
+    }
+}
