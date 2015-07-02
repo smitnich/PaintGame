c9afb02fa0541d5857bc040480e2187baa05529b
gerbias <smitnich@onid.orst.edu>
2015-07-01 17:31:02 -0700

    Script to automatically kill objects over time.

---
+++ Assets/LimitedLifetime.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LimitedLifetime : MonoBehaviour {
+    public int lifeTime = 2000;
+    int dieTime;
+	// Use this for initialization
+	void Start () {
+        dieTime = (int) (Time.time * 1000) + lifeTime;	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((Time.time * 1000) >= dieTime)
+            GameObject.Destroy(gameObject);
+	}
+}
