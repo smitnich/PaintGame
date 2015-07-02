e1dbf4a49ef86741e7f40e356418c2b2b6beb708
gerbias <smitnich@onid.orst.edu>
2015-07-01 17:29:41 -0700

    Make all enemies damage the player on collision.

--- Assets/BasicEnemy.cs
+++ Assets/BasicEnemy.cs
@@ -1,22 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BasicEnemy : MonoBehaviour {
+public class BasicEnemy : MonoBehaviour
+{
     public int health = 1;
     public int speed = 5;
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
     public void damage(int damage)
     {
         health -= damage;
         if (health <= 0)
             Destroy(gameObject);
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        PlayerMove playerScript = collision.gameObject.GetComponent<PlayerMove>();
+        if (playerScript != null)
+        {
+            GameObject.Destroy(collision.gameObject);
+            GameObject.Destroy(gameObject);
+        }
     }
 }
