05cca93ca7b924007faaa5e6fc3e2b5af56fa933
gerbias <smitnich@onid.orst.edu>
2016-02-09 22:06:44 -0800

    Add the ability to have objects be disabled for a period of time on first spawning.

---
+++ Assets/SpawnDisabled.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnDisabled : MonoBehaviour {
+
+    SetColor colorScript = null;
+    // We don't want a disabled object to move before
+    // its timer has run out; since there are many different
+    // ways it could move, we simply lock its position to its
+    // initial location
+    Vector3 lockPos;
+    public bool disabled = true;
+    public int timer = 1000;
+    int freeTime = 0;
+
+	// Use this for initialization
+	void Start () {
+        colorScript = (SetColor)GetComponent<SetColor>();
+        colorScript.ChangeColor(Color.black);
+        lockPos = transform.position;
+        freeTime = timer + ((int) Time.time * 1000);
+    }
+	
+    void LateUpdate() {
+        if (!disabled)
+            return;
+        if (Time.time * 1000 >= freeTime)
+        {
+            disabled = false;
+        }
+        if (disabled)
+        {
+            transform.position = lockPos;
+        }
+    }
+}
