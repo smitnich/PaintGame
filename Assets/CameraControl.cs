816d89370616d88a1aab6f88d7788c16d9875016
gerbias <smitnich@onid.orst.edu>
2016-03-04 13:37:10 -0800

    Upload assets.

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+    public GameObject toFollow;
+    public GameObject boundary;
+    Bounds floorBounds;
+    float xMin, xMax, yMin, yMax;
+
+	// Use this for initialization
+	void Start () {
+        floorBounds = boundary.GetComponent<Renderer>().bounds;
+        Vector3 max = floorBounds.max;
+        Vector3 min = floorBounds.min;
+        Camera cam = GetComponentInParent<Camera>();
+        float height = cam.orthographicSize;
+        float width = cam.orthographicSize * cam.aspect;
+        xMin = min.x + width;
+        xMax = max.x - width;
+        yMin = min.y + height;
+        yMax = max.y - height;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (toFollow == null)
+            return;
+        float xPos = Mathf.Clamp(toFollow.transform.position.x, xMin, xMax);
+        float yPos = Mathf.Clamp(toFollow.transform.position.y, yMin, yMax);
+        transform.position = new Vector3(xPos, yPos, transform.position.z);
+    }
+}
---
+++ Assets/SpawnEnemies.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemies : MonoBehaviour {
+
+    public GameObject[] spawnObjects = { null };
+    public int spawnRate = 4000;
+    public int spawnNumber = 10;
+    public int spawnDelay = 100;
+    public GameObject spawnWithin = null;
+    float maxX = 0;
+    float minX = 0;
+    float maxY = 0;
+    float minY = 0;
+    int lastSpawnTime = 0;
+    int spawnCount = 0;
+    bool spawning = false;
+    Vector3 spawnAt;
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        if (spawnWithin == null) {
+            spawnWithin = GameObject.Find("Floor");
+        }
+        if (player == null)
+        {
+            player = GameObject.Find("Player");
+        }
+        Bounds b = spawnWithin.GetComponent<Renderer>().bounds;
+        maxX = b.max.x;
+        maxY = b.max.y;
+        minY = b.min.y;
+        minX = b.min.x;
+    }
+	
+    Vector3 getRandomLocation()
+    {
+        return new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), -10);
+    }
+    void spawnObject()
+    {
+        GameObject newObj = Instantiate(spawnObjects[0]);
+        newObj.transform.position = spawnAt;
+    }
+    // Update is called once per frame
+    void Update () {
+        int curTime = (int)(Time.time * 1000);
+        if (spawning)
+        {
+            if (curTime >= spawnDelay + lastSpawnTime)
+            {
+                spawnObject();
+                spawnCount++;
+                if (spawnCount >= spawnNumber)
+                {
+                    spawning = false;
+                    spawnCount = 0;
+                }
+                lastSpawnTime = curTime;
+            }
+        }
+        else if (curTime >= spawnRate + lastSpawnTime)
+        {
+            spawnAt = getRandomLocation();
+            spawning = true;
+        }
+	}
+}
