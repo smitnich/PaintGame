7eae747dba6da30aded0b06841f3ab894f78244a
gerbias <smitnich@onid.orst.edu>
2016-03-06 22:32:02 -0800

    Implement basic version of bombs dropped by the player.

---
+++ Assets/DoExplosion.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoExplosion : MonoBehaviour {
+
+    private float finalRadius;
+    public float expandRate = 1.0f;
+    private float size = 1.0f;
+    private bool isPlayer;
+    public int damage = 1000;
+
+    public void StartExplosion(float radius, bool _isPlayer)
+    {
+        finalRadius = radius;
+        isPlayer = _isPlayer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        size += expandRate * Time.deltaTime;
+        transform.localScale = new Vector3(size, size, size);
+        if (size >= finalRadius)
+            GameObject.Destroy(gameObject);
+	}
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        BasicEnemy be = coll.gameObject.GetComponent<BasicEnemy>();
+        if (be != null)
+        {
+            be.damage(damage);
+        }
+    }
+}
---
+++ Assets/Explode.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explode : MonoBehaviour {
+
+    public float radius = 10;
+    public GameObject explosion = null;
+    public bool isPlayer = true;
+
+    public long bombDelay = 5000;
+    private long lastExplode = System.Int64.MinValue;
+
+	public void explode() {
+        long now = (long) Time.time * 1000;
+        // If it hasn't been the length of bombDelay since we last set a bomb,
+        // don't set one now
+        if (lastExplode + bombDelay > now)
+            return;
+        lastExplode = now;
+        GameObject obj = Instantiate(explosion);
+        obj.transform.position = gameObject.transform.position;
+        obj.GetComponent<DoExplosion>().StartExplosion(radius, isPlayer);
+	}
+}
